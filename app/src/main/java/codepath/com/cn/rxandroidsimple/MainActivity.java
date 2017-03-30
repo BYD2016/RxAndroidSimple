8edf8de63be223498bfffa8efa1f5b2d0bd37480
LiXiaoPing <17773406760@189.cn>
2017-03-30 13:24:32 +0800

    init

---
+++ app/src/main/java/codepath/com/cn/rxandroidsimple/MainActivity.java
@@ -0,0 +1,78 @@
+package codepath.com.cn.rxandroidsimple;
+
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public final class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private final CompositeDisposable disposables = new CompositeDisposable();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_run_scheduler).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                onRunSchedulerExampleButtonClicked();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        disposables.clear();
+        super.onDestroy();
+    }
+
+    private void onRunSchedulerExampleButtonClicked() {
+        disposables.add(sampleObservable()
+                // Run on a background thread
+                .subscribeOn(Schedulers.io())
+                // Be notified on the main thread
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<String>() {
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete()");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError()", e);
+                    }
+
+                    @Override
+                    public void onNext(String string) {
+                        Log.d(TAG, "onNext(" + string + ")");
+                    }
+                }));
+    }
+
+    private Observable<String> sampleObservable() {
+        return Observable.defer(new Callable<ObservableSource<? extends String>>() {
+
+            @Override
+            public ObservableSource<? extends String> call() throws Exception {
+                // Do some long running operation
+                SystemClock.sleep(5000);
+                return Observable.just("one", "two", "three", "four", "five");
+            }
+
+        });
+    }
+}
